ece434370d034e4c22c6aee27cbc6d7eac6775fc
Diogo Alves <diogo.alves.ti@gmail.com>
2021-11-06 20:21:12 -0300

    Add API documentation

--- orders/api.py
+++ orders/api.py
@@ -4,7 +4,7 @@ from fastapi import Depends, FastAPI, Request
 from fastapi.responses import JSONResponse
 
 from .exceptions import APIConnectionError, OrderNotFoundError
-from .schemas import Item
+from .schemas import ErrorResponse, HealthCheckResponse, Item
 from .services import get_order_items
 
 app = FastAPI()
@@ -26,11 +26,31 @@ def order_not_found_exception_handler(request: Request, exc: OrderNotFoundError)
     )
 
 
-@app.get('/healthcheck')
+@app.get(
+    path='/healthcheck',
+    tags=["healthcheck"],
+    summary='Integridade do sistema',
+    description='Checa se o servidor está online',
+    response_model=HealthCheckResponse)
 async def healthcheck():
-    return {'status': 'ok'}
+    return HealthCheckResponse(status='ok')
 
 
-@app.get('/orders/{order_id}/items')
+@app.get(
+    path='/orders/{order_id}/items',
+    responses={
+        HTTPStatus.NOT_FOUND.value: {
+            'description': 'Pedido não encontrado',
+            'model': ErrorResponse,
+        },
+        HTTPStatus.BAD_GATEWAY.value: {
+            'description': 'Falha na comunicação com o servidor remoto',
+            'model': ErrorResponse,
+        }
+    },
+    tags=["pedidos"],
+    summary='Itens de um pedido',
+    description='Retorna todos os itens de um determinado pedido',
+    response_model=list[Item])
 def list_items(items: list[Item] = Depends(get_order_items)):
     return items
